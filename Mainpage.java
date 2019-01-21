8b2b5331461f048d744cb50378028be973c4cd8a
pandeyketan <vibhu4dps2012@gmail.com>
2019-01-21 23:04:59 +0530

    test1

---
+++ Mainpage.java
@@ -0,0 +1,13 @@
+package com.ketan.second;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Mainpage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mainpage);
+    }
+}
